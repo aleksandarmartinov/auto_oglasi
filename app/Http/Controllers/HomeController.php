172205309ecd8a8dbdc5adc8c88f80114c417dd8
aleksandarmartinov <vrabac@DESKTOP-N9ITI2B.>
2025-01-28 10:41:14 +0100

    "Dodati view fajlovi, Car Controller i Factories"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $cars = Car::where('published_at', '<', now())
+            ->with(['primaryImage', 'city', 'carType', 'fuelType', 'maker', 'model'])
+            ->orderBy('published_at', 'desc')
+            ->limit(30)
+            ->get();
 
-        return view('home.index');
+        return view('home.index', ['cars' => $cars]);
     }
 }
